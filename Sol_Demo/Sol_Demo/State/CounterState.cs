f56db138e33f37c75c05c37e2d241889f41d2b24
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-13 20:27:26 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/State/CounterState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.State
+{
+    public interface ICounterState
+    {
+        event Action onCountChanged; 
+        int IncrementCount { get; set; }
+
+        void IncrementCouter();
+        
+    }
+
+    public class CounterState : ICounterState
+    {
+        public int IncrementCount { get; set; }
+
+        public event Action onCountChanged;
+
+        public void IncrementCouter()
+        {
+            IncrementCount++;
+            onCountChanged?.Invoke();
+        }
+    }
+}
